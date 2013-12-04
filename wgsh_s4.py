86e1488ae3c5a3eaa50f287ede4ac33f309ab2ff
akozik <akozik@gmail.com>
2013-12-04 04:55:27 +0000

    wgsh_s4 - define custom XR rules - first working version

--- wgsh_s4.py
+++ wgsh_s4.py
@@ -12,6 +12,9 @@ from os.path import basename, splitext
 
 #####################################################
 ''' 
+
+STANDARD / BASIC / ORIGINAL HAPLO-CALL RULE:
+ * see details under genotype_SR function *
 This script converts the group counts into group haplotypes with the GroupHaplotyper script.
 The haplotyping thresholds are hardcoded in the script and should be edited there at the top of the script.
 If the B/A call ratio is between 0.2 and 5, the group is typed as heterozygous;
@@ -21,14 +24,21 @@ If the number of het calls is equal or greater than the sum of A calls and B cal
 
 Usage: python  04-GroupHaplotyper.py  chunked-clean-genotype-table.tsv  number-of-samples genotype-call-rule 
 e.g:   python  04-GroupHaplotyper.py  genotype-table.tsv  88 SR 
+
+EXPERIMENTAL HAPLO-CALL RULE:
+ * see details under genotype_XR function *
+CUSTOMIZED TO HANDLE DATA WITH LOW NUMBER OF SNPs PER GROUP/BLOCK
+
 '''
-#####################################################
+
+######################################################
+#                        SR BEGIN                    #
 # B/A ratio between "min" and "max" --> resulting call: heterozygous
 # B/A ratio between smaller than or equal to "min" --> resulting call: A
 # B/A ratio between bigger than or equal to "max" --> resulting call: B
 
 min_SR = 0.2
-max_SR = 5
+max_SR = 5.0
 
 ######################################################
 #count in cells
@@ -58,20 +68,22 @@ def genotype_SR(groupsize, Vcnt, Ccnt, Mcnt, Ucnt):
 		GT = "-"
 	GTL = [GT]
 	return GTL
+#                        SR END                      #
 ######################################################
 
-# min_low_XR = 0.01
-# max_low_XR = 100
+######################################################
+#                        XR BEGIN                    #
 
-# min_med_XR = 0.05
-# max_med_XR = 20
+# CUTOFF RATIO FOR A-B SCORES #
+min_XR =  0.05
 
-min_high_XR = 0.1
-max_high_XR = 10
+#  CUTOFF RATIO FOR U SCORES  #
+min_UR = 0.10
 
-low__data_cutoff = 10
-med__data_cutoff = 50
-high_data_cutoff = 100
+# CUTOFF FOR LOW NUMBER OF SCORES #
+low__data_cutoff = 25
+# med__data_cutoff = 50
+# high_data_cutoff = 100
 
 def genotype_XR(groupsize, Vcnt, Ccnt, Mcnt, Ucnt):
 	Vcnt = float(Vcnt)
@@ -81,28 +93,87 @@ def genotype_XR(groupsize, Vcnt, Ccnt, Mcnt, Ucnt):
 	gs = int(groupsize)
 
 	GT = "X"
-
-	if Vcnt == 0 and Ccnt == 0:
+	##############################
+	### RANGE 1 - BELOW CUTOFF ###
+	if gs < low__data_cutoff:
+		GT = "?"
+		### CASE 0-0-X ###
+		if Vcnt == 0 and Ccnt == 0 and Ucnt == 0:
+			GT = "-"
+		if Vcnt == 0 and Ccnt == 0 and Ucnt >  0:
+			GT = "U"
+		### CASE A-0-X ###
+		if Vcnt == 0 and Ccnt >  0 and Ucnt == 0:
+			GT = "A"
+		if Vcnt == 0 and Ccnt >  0 and Ucnt >  0:
+			GT = "U"
+		### CASE 0-B-X ###
+		if Vcnt >  0 and Ccnt == 0 and Ucnt == 0:
+			GT = "B"
+		if Vcnt >  0 and Ccnt == 0 and Ucnt >  0:
+			GT = "U"
+		### CASE A-B-X ###
+		if Vcnt >  0 and Ccnt >  0 and Ucnt == 0:
+			GT = "U"
+		if Vcnt >  0 and Ccnt >  0 and Ucnt >  0:
+			GT = "U"
+	##############################
+	### RANGE 2 - ABOVE CUTOFF ###
+	if gs >= low__data_cutoff:
+		GT = "?"
+		### CASE 0-0-X ###
+		if Vcnt == 0 and Ccnt == 0 and Ucnt == 0:
+			GT = "-"
+		if Vcnt == 0 and Ccnt == 0 and Ucnt >  0:
+			GT = "U"
+		### CASE A-0-X ###
+		if Vcnt == 0 and Ccnt >  0 and Ucnt == 0:
+			GT = "A"
+		if Vcnt == 0 and Ccnt >  0 and Ucnt >  0:
+			GT = "A"
+			if Ucnt/Ccnt > min_UR:
+				GT = "U"
+		### CASE 0-B-X ###
+		if Vcnt >  0 and Ccnt == 0 and Ucnt == 0:
+			GT = "B"
+		if Vcnt >  0 and Ccnt == 0 and Ucnt >  0:
+			GT = "B"
+			if Ucnt/Vcnt > min_UR:
+				GT = "U"
+		### CASE A-B-X WITHOUT U ###
+		if Vcnt >  0 and Ccnt >  0 and Ucnt == 0:
+			if Vcnt == Ccnt:
+				GT = "U"
+			if Vcnt < Ccnt:
+				GT = "U"
+				if Vcnt/Ccnt < min_XR:
+					GT = "A"
+			if Vcnt > Ccnt:
+				GT = "U"
+				if Ccnt/Vcnt < min_XR:
+					GT = "B"
+		### CASE A-B-X  WITH U   ###
+		if Vcnt >  0 and Ccnt >  0 and Ucnt >  0:
+			if Vcnt == Ccnt:
+				GT = "U"
+			if Vcnt < Ccnt:
+				GT = "U"
+				if Vcnt/Ccnt < min_XR:
+					GT = "A"
+			if Vcnt > Ccnt:
+				GT = "U"
+				if Ccnt/Vcnt < min_XR:
+					GT = "B"
+			if Ucnt/(Vcnt+Ccnt) > min_UR:
+				GT = "U"
+	###############################
+	### TOO MANY MISSING SCORES ###
+	if Mcnt > 0.9 * gs:
 		GT = "-"
-	if Vcnt == 0 and Ccnt >  0:
-		GT = "A"
-	if Vcnt >  0 and Ccnt == 0:
-		GT = "B"
-	if Vcnt >  0 and Ccnt >  0:
-		# RANGE 1 - BELOW LOW #
-		if gs < low__data_cutoff:
-			GT = "?"
-		# RANGE 2 - BETWEEN LOW AND MEDIUM #
-		if gs >= low__data_cutoff and gs < med__data_cutoff:
-			GT = "?"
-		# RANGE 3 - BETWEEN MEDIUM AND HIGH #
-		if gs >= med__data_cutoff and gs < high_data_cutoff:
-			GT = "?"
-		# RANGE 4 - ABOVE HIGH #
-		if gs >= high_data_cutoff:
-			GT = "?"
 	GTL = [GT]
 	return GTL
+#                        XR END                      #
+######################################################
 
 #############
 # MAIN BODY #
